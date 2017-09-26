70fa4c509d3087d9a1f8f1bebf3434aed513aff1
Alexey <Fyodorov.aleksej@gmail.com>
2017-09-26 20:00:03 +0300

    change output
    changing output in lines

--- hdread.py
+++ hdread.py
@@ -8,7 +8,7 @@ logfile = open(os.getcwd() + "/log.txt")
 #create keys of information about hd disk
 keys = ["Model Number", "Firmware Revision", "Serial Number", "Transport", "DMA", "PIO"]
 #create dictianory with information about parameteres of hd disk
-dictianory = {k:None for k in keys}
+dictianory = {k: None for k in keys}
 for i in logfile:
     for j in keys:
         #if find substring (key + ":") in current line
@@ -36,15 +36,17 @@ for i in logfile:
 logfile.close()
 subprocess.call("df -hm | grep /dev/sda > " + os.getcwd() + "/log.txt", shell=True)
 logfile = open(os.getcwd() + "/log.txt")
-result = re.split(r" +",logfile.read())
-memdict = {"all":None, "free":None, "used":None}
+result = re.split(r" +", logfile.read())
+memdict = {"all": None, "free": None, "used": None}
 memdict["all"] = result[1]
 memdict["used"] = result[2]
 memdict["free"] = result[3]
-tempDict = {"Memory":memdict}
+tempDict = {"Memory": memdict}
 dictianory.update(tempDict)
 #print result dictianory
-print(dictianory)
+for k in dictianory.keys():
+    print(k + " : ")
+    print(dictianory[k])
 #remove temp file "/log.txt"
 path = os.path.join(os.path.abspath(os.path.dirname(__file__)), os.getcwd() + "/log.txt")
 os.remove(path)
